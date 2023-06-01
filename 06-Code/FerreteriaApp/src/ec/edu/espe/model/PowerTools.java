704ca85707eb3c882d825c9199c7b03241e4153e
Mesias Mariscal <mesiasv2@gmail.com>
2023-05-31 22:38:18 -0500

    ProjectV0.3

---
+++ 06-Code/FerreteriaApp/src/ec/edu/espe/model/PowerTools.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author Mesias Mariscal, CodeCrafters; DCCO-ESPE
+ */
+public class PowerTools {
+    private String name;
+    private float price;
+    private String type;
+
+    public PowerTools(String name, float price, String type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
---
+++ 06-Code/FerreteriaApp/src/ec/edu/espe/view/FerreteriaApp.java
@@ -0,0 +1,8 @@
+package ec.edu.espe.view;
+
+
+public class FerreteriaApp {
+    public static void main(String[] args) {
+
+}
+}
