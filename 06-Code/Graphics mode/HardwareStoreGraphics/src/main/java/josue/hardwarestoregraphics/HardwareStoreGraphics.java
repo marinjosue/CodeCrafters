582947b63d0450d68e6a75e09eca019c655b3034
marinjosue <josuisaac2002@gmail.com>
2023-07-09 19:30:34 -0500

    Update

---
+++ 06-Code/Graphics mode/HardwareStoreGraphics/src/main/java/josue/hardwarestoregraphics/HardwareStoreGraphics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package josue.hardwarestoregraphics;
+
+/**
+ *
+ * @author Josue Marin, CodeCrafters: DCCO-ESPE
+ */
+public class HardwareStoreGraphics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
