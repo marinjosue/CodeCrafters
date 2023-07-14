8504ce312021561a71ac105ea4b42f82d9102c18
Jeffry Manobanda <jnmanobanda@hotmail.com>
2023-07-14 18:44:51 -0500

    Save Facture

---
+++ 06-Code/Hardware store V 0.6.0-1/Hardware store V 0.6.0/Hardware store V 0.6.0/src/main/java/com/mycompany/project/Project.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project;
+
+
+/**
+ *
+ * @author Josue Marin, Jhordy Marcillo, Jeffrey Manobanda, Mesias Mariscal CodeCrafters: DCCO-ESPE
+ */
+import ec.edu.espe.controller.JSONDataManager;
+import ec.edu.espe.model.HardwareStore;
+import static com.mycompany.project.Owner.OWNER_PASSWORD;
+import static com.mycompany.project.Costomer.readInt;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Project {
+    public static void run() {
+        JSONDataManager jsonDataManager = new JSONDataManager();
+        HardwareStore hardwareStore = jsonDataManager.loadData();
+        showMainMenu(hardwareStore);
+    }
+   
+    
+public static void showMainMenu(HardwareStore hardwareStore) {
+    Scanner scanner = new Scanner(System.in);
+
+    boolean exit = false;
+    while (!exit) {
+               System.out.println("---------- BIENVENIDO A FERRETERIA DSA ----------");
+        System.out.println("DESEA INGRESAR COMO:");
+        System.out.println("1. Propietario");
+        System.out.println("2. Usuario");
+        System.out.println("3. Ver Comentarios del establecimiento");
+        System.out.println("4. Salir del sistema");
+        System.out.print("Ingrese la opcion deseada: ");
+        int op = readInt(scanner);
+        if (op == 1){
+            System.out.print("Ingrese la codigo personalde propietario: ");
+            scanner.nextLine(); 
+            String password = scanner.nextLine();
+            if (password.equals(OWNER_PASSWORD)) {
+               Owner.showOwnerMenu(hardwareStore, scanner);
+            } else {
+                System.out.println("Codigo incorrecta. Intente nuevamente.");
+                System.out.println("Presione Enter para continuar...");
+                try {
+                    System.in.read();
+                } catch (IOException e) {
+                }
+            }
+        } else if (op == 2) {
+            Costomer.showUserMenu(hardwareStore, scanner);
+        }
+            else if(op==3){
+             Costomer.showComments();
+            }else if(op ==4 ){
+                    System.out.println("Gracias :D");
+                    exit = true;
+                    break;
+        } else {
+            System.out.println("Opcion no valida. Intente nuevamente.");
+        }
+    }
+} 
+}
---
+++ 06-Code/Hardware store V 0.6.0-1/Hardware store V 0.6.0/Hardware store V 0.6.0/src/main/java/ec/edu/espe/model/HardwareStore.java
@@ -0,0 +1,160 @@
+
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author Josue Marin, Jhordy Marcillo, Jeffrey Manobanda, Mesias Mariscal CodeCrafters: DCCO-ESPE
+ */
+import ec.edu.espe.model.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HardwareStore {
+    private int id;
+    private String name;
+    private int batch;
+    private int amount;
+    private List<Product> productList;
+    private String email;
+    private Cart cart;
+
+    public HardwareStore() {
+        productList = new ArrayList<>();
+        cart = new Cart();
+    }
+
+    public HardwareStore(int id, String name, int batch, float amount1, String email) {
+        this.id = id;
+        this.name = name;
+        this.batch = batch;
+        this.amount = amount;
+        this.productList = productList;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "HardwareStore{" + "id=" + id + ", name=" + name + ", batch=" + batch + ", amount=" + amount + ", productList=" + productList + ", email=" + email + '}';
+    }
+
+    public void checkIn() {
+
+    }
+
+    public List<Product> consult(List<Product> productList) {
+
+        return null;
+    }
+
+    public void add(List<Product> productList) {
+
+    }
+
+    public void remove(List<Product> productList) {
+
+    }
+
+    public void modify(Product product) {
+
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the batch
+     */
+    public int getBatch() {
+        return batch;
+    }
+
+    /**
+     * @param batch the batch to set
+     */
+    public void setBatch(int batch) {
+        this.batch = batch;
+    }
+
+    /**
+     * @return the amount
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the productList
+     */
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    /**
+     * @param productList the productList to set
+     */
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+  public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public Object getProducts() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Object getEmployees() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
