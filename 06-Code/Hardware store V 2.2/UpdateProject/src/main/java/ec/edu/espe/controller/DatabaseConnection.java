4b7301828d0857b4436d47c43b9afaa6fa370ea5
Mesias Mariscal <mesiasv2@gmail.com>
2023-08-18 03:34:06 -0500

    Update_project

---
+++ 06-Code/Hardware store V 2.2/UpdateProject/src/main/java/ec/edu/espe/controller/DatabaseConnection.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ec.edu.espe.controller;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+
+/**
+ *
+ * @author Mesias Mariscal, CodeCrafters; DCCO-ESPE
+ */
+public class DatabaseConnection {
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+
+    public DatabaseConnection(String collectionName) {
+        mongoClient = MongoClients.create("mongodb+srv://josuemarin:josuemarin@cluster0.lntjz9j.mongodb.net/");
+        database = mongoClient.getDatabase("Project");
+        collection = database.getCollection(collectionName);
+    }
+
+    public MongoCollection<Document> getCollection() {
+        return collection;
+    }
+    
+
+
+}
+
